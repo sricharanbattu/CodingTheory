0755e669a38b00218decf86afbbe6c3b9d2a9ba8
Sri Charan Battu <34030057+sricharanbattu@users.noreply.github.com>
2022-09-01 23:56:24 +0530

    Update defines.h

--- CodeGeneration/defines.h
+++ CodeGeneration/defines.h
@@ -3,5 +3,5 @@
 #define DEFINES_H
 
 #define ASSERT_TOTAL_PROB(prob_sum,eps,message)		assert((abs(prob_sum-1) < eps) && message)
-#define ASSERT_RANGE_PROB(prob, a, b, message)			assert((prob > a) && (prob <= b) && message)
+#define ASSERT_PROB_RANGE(prob, a, b, message)			assert((prob > a) && (prob <= b) && message)
 #endif 
