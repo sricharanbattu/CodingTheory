da93452229fbae785baf18bffcb25b217a93ddf4
sricharanbattu <sricharanbattu@gmail.com>
2022-09-12 00:38:36 +0530

    trivial

--- CodeGeneration/utilities.cpp
+++ CodeGeneration/utilities.cpp
@@ -147,17 +147,26 @@ void generateCodes(vector<double>& probs, vector<string>& codes, codingStrategy_
 
 
 // Print entropy, codes and avg code length for a given distribution and codes. 
+void printProbAndCode(vector<double>& probs, vector<string>& codes)
+{
+
+	int n = probs.size();
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Code word for the message with probability " << probs[i] << " is : \t" << codes[i] << '\n';
+	}
+	
+}
 
 void printDetails(vector<double>& probs, vector<string>& codes)
 {
 
 	int n = probs.size();
 	//cout << std::setprecision(5);
-	for (int i = 0; i < n; i++)
-	{
-		cout << "Code word for the message with probability " << probs[i] << " is : \t" << codes[i] << '\n';
-	}
+	
+	printProbAndCode(probs, codes);
 	doubleLine();
+	
 
 	cout << "Entropy of the message                    =   " << getEntropy(probs) << '\n';
 	cout << "Average Code Length of the message        =   " << getAverageCodeLength(probs, codes) << '\n';
--- CodeGeneration/utilities.h
+++ CodeGeneration/utilities.h
@@ -16,11 +16,11 @@ double getRandomDouble();
 void getRandomProbabilities(vector<double>& probs);
 void getInputProbabilities(vector<double>& probs);
 void assertProbabilities(vector<double>& probs);
+void printProbAndCode(vector<double>& probs, vector<string>& codes);
 void printProbabilities(vector<double>& probs);
 
 double getEntropy(vector<double>& probs);
 double getAverageCodeLength(vector<double>& probs, vector<string>& codes);
-void getCodes(vector<double>& sorted_probs,vector<string>& codes, const enum class CodeMethod& code_method); // const references can take rvalues
 void generateCodes(vector<double>& probs, vector<string>& codes, codingStrategy_t codeStrategy);
 void printDetails(vector<double>& probs, vector<string>& codes);
 
